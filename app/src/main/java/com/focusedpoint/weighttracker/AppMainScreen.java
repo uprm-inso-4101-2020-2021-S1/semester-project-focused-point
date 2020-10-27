ee44e0b87ccb26582dc784e5720a315190bbadf6
luisgonz09 <luis.gonzalez119@upr.edu>
2020-10-27 17:52:56 -0400

    Sign Up and Main Screen UI added

---
+++ app/src/main/java/com/focusedpoint/weighttracker/AppMainScreen.java
@@ -0,0 +1,16 @@
+package com.focusedpoint.weighttracker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AppMainScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_app_main_screen);
+
+
+    }
+}

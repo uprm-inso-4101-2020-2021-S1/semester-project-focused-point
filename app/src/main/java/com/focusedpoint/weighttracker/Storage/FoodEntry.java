920733f41f42979cd1d266cc226ba81caa75d224
VictorBaezM <victor.baez4@upr.edu>
2020-10-28 11:13:16 -0400

    Made changes to the following classes:
    ConvertableToString- new interface that forces an object to have a toString() method.
    Date,FoodEntry,WeightEntry,User - made changes to make the access of information easier.
    ArrayList,HashTableSC - made these data structures implement Convertable ToString
    User - added file write and load methods

--- app/src/main/java/com/focusedpoint/weighttracker/Storage/FoodEntry.java
+++ app/src/main/java/com/focusedpoint/weighttracker/Storage/FoodEntry.java
@@ -1,19 +1,31 @@
 package com.focusedpoint.weighttracker.Storage;
 
 import com.focusedpoint.weighttracker.DataStructures.list.ArrayList;
+import com.focusedpoint.weighttracker.DataStructures.list.ConvertableToString;
 
 
-public class FoodEntry extends Date {
+public class FoodEntry extends Date implements ConvertableToString {
 
     private double Calories;
     private ArrayList<String> TimesEaten; //List that contains the dates the food entry is eaten. It saves them in the following format (day/month/year).
     public  FoodEntry(double calories){
         this.Calories=calories;
         TimesEaten = new ArrayList<String>(30);
+        addItemToEaten();
+
+    }
+    public  FoodEntry(double calories,ArrayList<String> TimesEaten){
+        this.Calories=calories;
+        this.TimesEaten = TimesEaten;
+
     }
 
     public void addItemToEaten(){
         TimesEaten.add(currentDate());
+    }
+
+    public void addItemToEaten(String TimeEaten){
+        TimesEaten.add(TimeEaten);
     }
 
     public double getCalories() {
@@ -30,5 +42,16 @@ public class FoodEntry extends Date {
 
     public void setTimesEaten(ArrayList<String> timesEaten) {
         TimesEaten = timesEaten;
+    }
+
+    @Override
+    public String toString() {
+        String result = "Calories("+Calories+")"+"TimesEaten[";
+        for(String s:TimesEaten){
+            if(s!=null)
+                result=result+s+",";
+        }
+        result+="]\n";
+        return result;
     }
 }

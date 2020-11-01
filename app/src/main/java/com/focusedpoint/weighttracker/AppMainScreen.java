ae3df4e9262961736412994918aa1039c0bec125
cernesto99 <carlos.cardona11@upr.edu>
2020-10-31 18:27:53 -0700

    Made more buttons work

--- app/src/main/java/com/focusedpoint/weighttracker/AppMainScreen.java
+++ app/src/main/java/com/focusedpoint/weighttracker/AppMainScreen.java
@@ -2,8 +2,11 @@ package com.focusedpoint.weighttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.focusedpoint.weighttracker.ui.login.LoginActivity;
 
 public class AppMainScreen extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class AppMainScreen extends AppCompatActivity {
 
     // Defines what happens when user clicks on "Track My Weight" Button
     public void openWeight(View view) {
+        startActivity(new Intent(AppMainScreen.this, MainActivity.class));
 
     }
 
--- app/src/main/java/com/focusedpoint/weighttracker/SignUpActivity.java
+++ app/src/main/java/com/focusedpoint/weighttracker/SignUpActivity.java
@@ -2,8 +2,11 @@ package com.focusedpoint.weighttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+
+import com.focusedpoint.weighttracker.ui.login.LoginActivity;
 
 public class SignUpActivity extends AppCompatActivity {
 
@@ -15,12 +18,13 @@ public class SignUpActivity extends AppCompatActivity {
 
     // Defines what happens when back button is clicked;
     public void goback(View view) {
+        startActivity(new Intent(SignUpActivity.this, LoginActivity.class));
 
     }
 
     // Defines what happens when user clicks on "Sign Up Button"
     public void createUser(View view) {
-        
+        startActivity(new Intent(SignUpActivity.this, AppMainScreen.class));
     }
 
 }

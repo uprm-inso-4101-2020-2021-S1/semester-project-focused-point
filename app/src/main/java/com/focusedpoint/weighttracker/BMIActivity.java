ea766d232c296ae439089706690a05c08bb95a7f
luisgonz09 <luis.gonzalez119@upr.edu>
2020-11-05 11:16:23 -0400

    BACK buttons activated on FMI/BMI UI screens

--- app/src/main/java/com/focusedpoint/weighttracker/BMIActivity.java
+++ app/src/main/java/com/focusedpoint/weighttracker/BMIActivity.java
@@ -2,6 +2,7 @@ package com.focusedpoint.weighttracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
@@ -15,6 +16,6 @@ public class BMIActivity extends AppCompatActivity {
 
     // defines what happens when back button is pressed; user will be returned to main screen
     public void backtoMain(View view) {
-
+        startActivity(new Intent(BMIActivity.this, AppMainScreen.class));
     }
 }
